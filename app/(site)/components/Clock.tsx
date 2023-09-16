d0e0537bb853ab66c9e8522d1ca62ab369708e36
liwenkai <2020583117@qq.com>
2023-09-17 00:18:00 +0800

    添加日期

--- app/(site)/components/Clock.tsx
+++ app/(site)/components/Clock.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
 const Clock = () => {
   const [hours, setHours] = useState('00')
@@ -30,15 +30,28 @@ const Clock = () => {
       setSeconds(ss)
     }
   }
+  const currentDate = useMemo(() => {
+    const currentDate = new Date()
+    const date = currentDate.getDate()
+    const month = currentDate.getMonth() + 1
+    const year = currentDate.getFullYear()
+    const day = currentDate.getDay()
+    const daysOfWeek = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+    return { date, month, year, day, daysOfWeek }
+  }, [])
 
   return (
-    <div className="rounded-lg px-10 flex text-white text-7xl sm:text-9xl">
-      <span>
+    <div className="rounded-lg flex flex-col items-center justify-center text-white text-7xl sm:text-9xl w-full">
+      <p>
+        {currentDate.year + '-' + currentDate.month + '-' + currentDate.date}
+        {currentDate.daysOfWeek[currentDate.day]}
+      </p>
+      <p>
         {hours[0]}
         {hours[1]}:{minutes[0]}
         {minutes[1]}:{seconds[0]}
         {seconds[1]}
-      </span>
+      </p>
     </div>
   )
 }

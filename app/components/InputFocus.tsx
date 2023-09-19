b92b328435d675bf3de309fb6dcfd0c466edcc43
liwenkai <2020583117@qq.com>
2023-09-19 16:28:13 +0800

    搜索框默认中间开始输入

--- app/components/InputFocus.tsx
+++ app/components/InputFocus.tsx
@@ -14,7 +14,7 @@ const InputFocus = () => {
     <div className="absolute top-24 max-w-[680px] w-[calc(100%-60px)] flex flex-row">
       <Input
         type="text"
-        className="backdrop-blur-md bg-white text-black focus:outline-0 focus:outline-white focus:leading-10 focus:align-middle rounded-full"
+        className="backdrop-blur-md bg-white text-black focus:outline-0 focus:outline-white focus:leading-10 focus:align-middle rounded-full text-center"
         ref={inputFocusRef}
       />
       <div className="absolute h-9 w-12 flex items-center justify-center cursor-pointer rounded-full hover:bg-black hover:opacity-50 text-gray-400">

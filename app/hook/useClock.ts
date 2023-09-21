9be295d784ce421cd05322a715a22a2f53360ea4
liwenkai <2020583117@qq.com>
2023-09-21 15:55:54 +0800

    优化时间展示逻辑

--- app/hook/useClock.ts
+++ app/hook/useClock.ts
@@ -6,29 +6,30 @@ export const useClock = () => {
   const [hours, setHours] = useState('00')
   const [minutes, setMinutes] = useState('00')
   const [seconds, setSeconds] = useState('00')
+
   useEffect(() => {
+    const updateTime = () => {
+      const now = new Date()
+      const hh = String(now.getHours()).padStart(2, '0')
+      const mm = String(now.getMinutes()).padStart(2, '0')
+      const ss = String(now.getSeconds()).padStart(2, '0')
+
+      if (hh !== hours) {
+        setHours(hh)
+      }
+
+      if (mm !== minutes) {
+        setMinutes(mm)
+      }
+
+      if (ss !== seconds) {
+        setSeconds(ss)
+      }
+    }
+
     const interval = setInterval(updateTime, 1000)
     return () => clearInterval(interval)
-  })
-
-  const updateTime = () => {
-    const now = new Date()
-    const hh = String(now.getHours()).padStart(2, '0')
-    const mm = String(now.getMinutes()).padStart(2, '0')
-    const ss = String(now.getSeconds()).padStart(2, '0')
-
-    if (hh !== hours) {
-      setHours(hh)
-    }
-
-    if (mm !== minutes) {
-      setMinutes(mm)
-    }
-
-    if (ss !== seconds) {
-      setSeconds(ss)
-    }
-  }
+  }, [hours, minutes, seconds])
 
   const [weather, setWeather] = useState<weather | null>(null)
   axios

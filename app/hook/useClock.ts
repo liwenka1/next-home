7749a4cf0c5b2fbfd2bb46d0a3b3a614956c39d5
liwenkai <2020583117@qq.com>
2023-09-27 10:41:43 +0800

    测试ip接口能否使用

--- app/hook/useClock.ts
+++ app/hook/useClock.ts
@@ -21,6 +21,7 @@ export const useClock = () => {
   const [weather, setWeather] = useState<weather | null>(null)
   const [weatherIcon, setWeatherIcon] = useState<string>('images/weather-animation-icon/not-available.svg')
   useEffect(() => {
+    axios.get('/api/ip').then((res) => console.log(res))
     axios
       .post('/api/weather', { adcode: '110000' })
       .then((res) => {
